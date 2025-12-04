31604037dfd9cca98a4f11e1d8e26fc1f9900aad
MAZZOLENI.22138 <mazzoleni.federico.studente@itispaleocapa.it>
2025-12-04 08:50:07 +0100

    fineeee

--- Test.java
+++ Test.java
@@ -2,9 +2,9 @@ public class Test {
     public static void main() {
         RegistroColloqui registro = new RegistroColloqui();
 
-        Colloquio c1 = new Colloquio("Deyae", "Nawal", "3A", 1);
-        Colloquio c2 = new Colloquio("Giacomo", "Roberto", "3B", 2);
-        Colloquio c3 = new Colloquio("Ilario", "Pamela", "3C", 3);
+        Colloquio c1 = new Colloquio("Deyae", "Nawal", "4IE", 1);
+        Colloquio c2 = new Colloquio("Giacomo", "Roberto", "4IE", 2);
+        Colloquio c3 = new Colloquio("Ilario", "Pamela", "4IE", 3);
 
         registro.aggiungiColloquio(c1);
         registro.aggiungiColloquio(c2);
